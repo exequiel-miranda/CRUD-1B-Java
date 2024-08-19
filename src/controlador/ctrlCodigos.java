0df73d82d70cc3330124872b25d7cd71b449cf84
Daniel Granados <20200008@ricaldone.edu.sv>
2024-08-19 13:42:41 -0600

    initial commit

---
+++ src/controlador/ctrlCodigos.java
@@ -0,0 +1,52 @@
+package controlador;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import modelo.Codigos;
+import vista.frmCodigos;
+
+public class ctrlCodigos implements MouseListener{
+    
+    private Codigos modelo;
+    private frmCodigos vista;
+    
+    public ctrlCodigos(Codigos modelo, frmCodigos vista){
+        this.modelo = modelo;
+        this.vista = vista;
+        
+        vista.btnAgregar.addMouseListener(this);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        
+        if(e.getSource() == vista.btnAgregar){
+            modelo.setNombre_estudiante(vista.txtNombre.getText());
+            modelo.setCarnet_estudiante(Integer.parseInt(vista.txtCarnet.getText()));
+            modelo.setTipo_Codigo(vista.txtTipoCodigo.getText());
+            
+            modelo.Guardar();
+                    
+
+        }
+        
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+    
+    
+}
